6ecdc02213e20ccc9abfaddfdbc2231fad1b033a
korschunoffk <33195448+korschunoffk@users.noreply.github.com>
2018-02-22 09:12:21 +0300

    сборщик готов

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+"use strict";
+
+var gulp = require("gulp");
+var less = require("gulp-less");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin  = require("gulp-imagemin");
+var del = require("del");
+var run = require("run-sequence");
+
+gulp.task("images", function() {
+  return gulp.src("source/img/*.{png,jpg,svg}")
+    .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.jpegtran({progressive: true}),
+    imagemin.svgo()
+  ]))
+  .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("style", function() {
+  gulp.src("source/less/style.less")
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("source/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("source/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("serve", ["style"], function() {
+  server.init({
+    server: "source/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/less/**/*.less", ["style"]);
+  gulp.watch("source/*.html").on("change", server.reload);
+});
+
+gulp.task("copy", function (){
+  return gulp.src([
+    "source/fonts/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.html",
+    "source/css/*.css"
+  ],{
+    base: "source"
+  })
+  .pipe(gulp.dest("build"))
+});
+
+gulp.task("clean", function (){
+  return del("build");
+});
+
+gulp.task("build", function (done){
+  run(
+    "clean",
+    "style",
+    "copy",
+    done
+  );
+});
